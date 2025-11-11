0796f7822fbde16c0535534f1b4a098faa9d2ee9
Shubham Midgule <shubham.midgule17@gmail.com>
2025-11-11 22:06:33 +0530

    Add component library with initial UI components and configuration
    - Introduced a new `components.json` configuration file for component styling and aliases.
    - Added multiple UI components including Accordion, AlertDialog, Avatar, Button, Card, Checkbox, and more.
    - Updated `package.json` and `package-lock.json` to include new dependencies for Radix UI components and other utilities.
    - Enhanced global styles in `globals.css` to support new components and themes.
    - Implemented utility functions in `utils.ts` for class name management.
    - Created a custom hook `useIsMobile` for responsive design handling.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>Hello World</div>
+    <div className='min-h-screen min-w-screen flex items-center justify-center'>
+      <Button>
+        Click me
+      </Button>
+    </div>
   )
 }
 
